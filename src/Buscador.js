4b0593f1fc46dcdc95f8be36e9a580fc1fb090d5
Zuchovicki <C9946641@eaton.ad.etn.com>
2022-07-27 14:15:50 -0500

    Added : Table and Table-Searcher

--- src/Buscador.js
+++ src/Buscador.js
@@ -1,17 +1,23 @@
-import React, { Component } from "react";
+
 import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 function Buscador() {
     const [data, setData] = useState([]);
-
+    const [filtereddata, setfilteredData] = useState([]);
+    const [filter, setfilter] = useState("");
+    
     useEffect(() => {
       (async () => {
-        const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
+        const result = await axios("https://jsonplaceholder.typicode.com/users");
         setData(result.data);
+        setfilteredData(result.data)
       })();
     }, []);
-
+    
+  
+    
+    
     return (
 
         <div>
@@ -19,13 +25,35 @@ function Buscador() {
 
            
 
-            <input type="text" name="Busqueda" ></input>
+            <input type="text" name="Busqueda" onChange={event => {
+                setfilter(event.target.value)
+                const newData = data.filter(function(item){
+                    const itemData = item.name.toUpperCase()
+                    const textData = filter.toUpperCase()
+                    return itemData.indexOf(textData) > -1})
+                setfilteredData(newData)
+                }}></input>
             <div>
                 <h4>Resultados de Busqueda</h4>
             </div>
 
-        
+        <table>
+        <thead>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+        </thead>
+        {filtereddata.map((val, key) => {
+          return (
+            <tbody key={key}>
+              <td>{val.name}</td>
+              <td>{val.username}</td>
+              <td>{val.email}</td>
+            </tbody>
+          )
+        })}
 
+        </table>
         </div>
     )
 }
